e71b395ee029c6f6ee8adc032c017e08181018b1
asfman <asfman@gmail.com>
2024-07-19 15:46:56 +0800

    chore:game_ids remove parseInt

--- components/Content.tsx
+++ components/Content.tsx
@@ -36,7 +36,7 @@ export function Content() {
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 p-4">
         {
           GAME_IDS.map((id) => (
-            <Game key={id} gameId={parseInt(id, 10)} />
+            <Game key={id} gameId={id} />
           ))
         }
       </div>
