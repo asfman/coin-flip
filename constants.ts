eb7b732c2572ff844e39dccfd9ea73d4f38038da
asfman <asfman@gmail.com>
2024-07-19 15:30:00 +0800

    feature:add flip coin page

---
+++ constants.ts
@@ -0,0 +1,5 @@
+import { Network } from "@aptos-labs/ts-sdk";
+export const NETWORK = Network.MAINNET;
+export const ACCOUNT_ADDRESS = "0x66fd6237b7f79a1914ec07e9fc9e54c730b732eac1646cbc42d19eb57b0df780";
+export const APT_COIN = "0x1::aptos_coin::AptosCoin";
+export const FLIP_MODULE_NAME = "coin_flip";
